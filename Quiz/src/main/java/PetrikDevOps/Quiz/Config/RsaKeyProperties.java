cd297a50a73d5ff56a1153aac8f6c9ee66ca98b0
Preisler <preisler.andras@gmail.com>
2023-02-24 08:03:08 +0100

    jwt

--- Quiz/src/main/java/PetrikDevOps/Quiz/Config/RsaKeyProperties.java
+++ Quiz/src/main/java/PetrikDevOps/Quiz/Config/RsaKeyProperties.java
@@ -2,6 +2,9 @@ package PetrikDevOps.Quiz.Config;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
+import java.security.interfaces.RSAPrivateKey;
+import java.security.interfaces.RSAPublicKey;
+
 @ConfigurationProperties(prefix = "rsa")
-public record RsaKeyProperties() {
+public record RsaKeyProperties(RSAPrivateKey privateKey, RSAPublicKey publicKey) {
 }
